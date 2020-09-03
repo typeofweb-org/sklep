55099cf41b049181221de180d4793b50103d03a2
WolfOfProgramming <bartekfc17@gmail.com>
2020-09-03 15:42:51 +0200

    Added Admin Folder in components and Admin route

---
+++ apps/www/pages/admin/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function AdminHome() {
+  return (
+    <>
+      <Head>
+        <title>Admin</title>
+      </Head>
+      {/* Pass here your content */}
+      <div></div>
+    </>
+  );
+}
