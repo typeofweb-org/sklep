2dd6a231a9d4da00843823861b9bea028ac43066
Michal Lester <michal.lester@allegro.pl>
2020-09-05 12:08:19 +0200

    rmv getStaticProps from cart page

--- apps/www/pages/koszyk/index.tsx
+++ apps/www/pages/koszyk/index.tsx
@@ -1,4 +1,3 @@
-import { InferGetStaticPropsType } from 'next';
 import React from 'react';
 
 import { Cart } from '../../components/klient/modules/cart/Cart';
@@ -6,7 +5,10 @@ import { Header } from '../../components/klient/shared/header/Header';
 import { Layout } from '../../components/klient/shared/layout/Layout';
 import { Order } from '../../types/order';
 
-function CartPage({ order }: InferGetStaticPropsType<typeof getStaticProps>) {
+function CartPage() {
+  const order: Order = {
+    id: '345',
+  };
   return (
     <Layout title="Sklep strona główna">
       <Header />
@@ -14,20 +16,5 @@ function CartPage({ order }: InferGetStaticPropsType<typeof getStaticProps>) {
     </Layout>
   );
 }
-export const getStaticProps = async () => {
-  // const res = await fetch('https://.../')
-
-  // todo: change with api call result
-  const order: Order = {
-    id: '345',
-  };
-
-  return {
-    props: {
-      order,
-    },
-    revalidate: 1, // In seconds
-  };
-};
 
 export default CartPage;
