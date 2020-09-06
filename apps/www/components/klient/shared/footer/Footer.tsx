574f7335ead00d04e4afc46923697f5cb004a8a4
Pawel Chyla <chyla1988@gmail.com>
2020-09-06 19:08:03 +0200

    Prepare footer style and structure

--- apps/www/components/klient/shared/footer/Footer.tsx
+++ apps/www/components/klient/shared/footer/Footer.tsx
@@ -1,34 +1,29 @@
+import Link from 'next/Link';
 import React from 'react';
 
 export const Footer = React.memo(() => (
-  <footer className="w-full bg-white py-8 border-t border-gray-400">
-    <div className="container mx-auto flex px-3 py-8 ">
-      <div className="w-full mx-auto flex flex-wrap">
-        <div className="flex w-full lg:w-1/2 ">
-          <div className="px-3 md:px-0">
-            <h3 className="font-bold text-gray-900">About</h3>
-            <p className="py-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis
-              tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
-            </p>
-          </div>
-        </div>
-        <div className="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
-          <div className="px-3 md:px-0">
-            <h3 className="font-bold text-gray-900">Social</h3>
-            <ul className="list-reset items-center pt-3">
-              <li>
-                <a
-                  className="inline-block no-underline hover:text-black hover:underline py-1"
-                  href="#"
-                >
-                  Add social links
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+  <footer className="w-full bg-white py-16 border-t border-gray-400">
+    <div className="container mx-auto flex px-6 mb-4 flex flex-wrap justify-between">
+      <div className="w-full lg:w-1/5 px-3 md:px-0">
+        <h3 className="font-bold text-gray-900 mb-4">Najnowsze Newsy</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus
+          commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
+        </p>
+      </div>
+      <div className="w-full lg:w-1/5 px-3 md:px-0">
+        <h3 className="font-bold text-gray-900 mb-4">Linki</h3>
+        <Link href="/">
+          <a>Strona głowna</a>
+        </Link>
+      </div>
+      <div className="w-full lg:w-1/5 px-3 md:px-0">
+        <h3 className="font-bold text-gray-900 mb-4">Śledź nas</h3>
+      </div>
+      <div className="w-full lg:w-1/5 px-3 md:px-0">
+        <h3 className="font-bold text-gray-900 mb-4">Newsletter</h3>
       </div>
     </div>
+    <p className="text-center">Copyright © 2020 Sklep TypeOfWeb</p>
   </footer>
 ));
