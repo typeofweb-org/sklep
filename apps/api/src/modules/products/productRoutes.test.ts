92fc9cd1ed9f7a874907e3642054c817c3bca837
Michał Miszczyszyn <mmiszy@users.noreply.github.com>
2020-09-07 21:33:26 +0200

    Edit delete product (#43)
    * Fixes #5. Edit and delete products
    
    * Updates
    
    * Ignore types
    
    * Revert

--- apps/api/src/modules/products/productRoutes.test.ts
+++ apps/api/src/modules/products/productRoutes.test.ts
@@ -15,11 +15,11 @@ describe('/products', () => {
         url: '/products',
         headers: auth.headers,
         payload: {
-          name: Faker.commerce.productName(),
+          name: Faker.lorem.sentence(5),
           description: Faker.lorem.sentences(5),
           isPublic: Faker.random.arrayElement([true, false]),
-          regularPrice: Faker.commerce.price(50, 100),
-          discountPrice: Faker.commerce.price(10, 49),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
           type: Enums.ProductType.SINGLE,
         },
       });
@@ -38,7 +38,7 @@ describe('/products', () => {
         payload: {
           description: Faker.lorem.sentences(5),
           isPublic: Faker.random.arrayElement([true, false]),
-          discountPrice: Faker.commerce.price(10, 49),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
           type: Enums.ProductType.SINGLE,
         },
       });
@@ -55,16 +55,154 @@ describe('/products', () => {
         url: '/products',
         headers: auth.headers,
         payload: {
-          name: Faker.commerce.productName(),
+          name: Faker.lorem.sentence(5),
           description: Faker.lorem.sentences(5),
           isPublic: Faker.random.arrayElement([true, false]),
-          regularPrice: Faker.commerce.price(50, 100),
-          discountPrice: Faker.commerce.price(10, 49),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
           type: Enums.ProductType.SINGLE,
         },
       });
 
       expect(injection.statusCode).toEqual(403);
+    });
+  });
+
+  describe('PUT', () => {
+    it(`should update a product with new data`, async () => {
+      const server = await getServerForTest();
+      const auth = await createAndAuthRole(server, Enums.UserRole.ADMIN);
+
+      const injection1 = await server.inject({
+        method: 'POST',
+        url: '/products',
+        headers: auth.headers,
+        payload: {
+          name: Faker.lorem.sentence(5),
+          description: Faker.lorem.sentences(5),
+          isPublic: Faker.random.arrayElement([true, false]),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
+          type: Enums.ProductType.SINGLE,
+        },
+      });
+      const { data } = injection1.result as SklepTypes['postProducts200Response'];
+
+      const newData = {
+        name: Faker.lorem.sentence(5),
+        description: Faker.lorem.sentences(5),
+        isPublic: Faker.random.arrayElement([true, false]),
+        regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+        discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
+        type: Enums.ProductType.SINGLE,
+      };
+
+      const injection = await server.inject({
+        method: 'PUT',
+        url: `/products/${data.id}`,
+        headers: auth.headers,
+        payload: newData,
+      });
+
+      expect(injection.statusCode).toEqual(200);
+
+      const productInDb = await server.app.db.product.findOne({ where: { id: data.id } });
+      expect(productInDb).toMatchObject(newData);
+    });
+    it(`should not update a product with invalid data`, async () => {
+      const server = await getServerForTest();
+      const auth = await createAndAuthRole(server, Enums.UserRole.ADMIN);
+
+      const injection1 = await server.inject({
+        method: 'POST',
+        url: '/products',
+        headers: auth.headers,
+        payload: {
+          name: Faker.lorem.sentence(5),
+          description: Faker.lorem.sentences(5),
+          isPublic: Faker.random.arrayElement([true, false]),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
+          type: Enums.ProductType.SINGLE,
+        },
+      });
+      const { data } = injection1.result as SklepTypes['postProducts200Response'];
+
+      const injection = await server.inject({
+        method: 'PUT',
+        url: `/products/${data.id}`,
+        headers: auth.headers,
+        payload: {
+          name: Faker.lorem.sentence(5),
+          description: Faker.lorem.sentences(5),
+          isPublic: Faker.random.arrayElement([true, false]),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
+          type: 'DUPA',
+        },
+      });
+
+      expect(injection.statusCode).toEqual(400);
+    });
+
+    it(`should not update a non-existent product`, async () => {
+      const server = await getServerForTest();
+      const auth = await createAndAuthRole(server, Enums.UserRole.ADMIN);
+
+      const injection = await server.inject({
+        method: 'PUT',
+        url: `/products/123123123123`,
+        headers: auth.headers,
+        payload: {
+          name: Faker.lorem.sentence(5),
+          description: Faker.lorem.sentences(5),
+          isPublic: Faker.random.arrayElement([true, false]),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
+          type: Enums.ProductType.SINGLE,
+        },
+      });
+
+      expect(injection.statusCode).toEqual(404);
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should delete a product', async () => {
+      const server = await getServerForTest();
+      const auth = await createAndAuthRole(server, Enums.UserRole.ADMIN);
+
+      const injection1 = await server.inject({
+        method: 'POST',
+        url: '/products',
+        headers: auth.headers,
+        payload: {
+          name: Faker.lorem.sentence(5),
+          description: Faker.lorem.sentences(5),
+          isPublic: Faker.random.arrayElement([true, false]),
+          regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+          discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
+          type: Enums.ProductType.SINGLE,
+        },
+      });
+      const { data } = injection1.result as SklepTypes['postProducts200Response'];
+
+      await server.inject({
+        method: 'DELETE',
+        url: `/products/${data.id}`,
+        headers: auth.headers,
+      });
+    });
+
+    it('should delete a non-existent product', async () => {
+      const server = await getServerForTest();
+      const auth = await createAndAuthRole(server, Enums.UserRole.ADMIN);
+
+      await server.inject({
+        method: 'DELETE',
+        url: `/products/12312312333`,
+        headers: auth.headers,
+      });
     });
   });
 
@@ -79,11 +217,11 @@ describe('/products', () => {
           url: '/products',
           headers: auth.headers,
           payload: {
-            name: Faker.commerce.productName(),
+            name: Faker.lorem.sentence(5),
             description: Faker.lorem.sentences(5),
             isPublic: Faker.random.arrayElement([true, false]),
-            regularPrice: Faker.commerce.price(50, 100),
-            discountPrice: Faker.commerce.price(10, 49),
+            regularPrice: parseInt(Faker.commerce.price(50, 100), 10),
+            discountPrice: parseInt(Faker.commerce.price(10, 49), 10),
             type: Enums.ProductType.SINGLE,
           },
         }),
