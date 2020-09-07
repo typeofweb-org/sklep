b770bf6da7c4bacffddd250d66c0a453cae7afd7
jundymek <jundymek@gmail.com>
2020-09-07 23:39:13 +0200

    reafactor: pr 20#41 fixes

--- apps/www/components/klient/modules/productCollection/ProductCollection.tsx
+++ apps/www/components/klient/modules/productCollection/ProductCollection.tsx
@@ -4,8 +4,9 @@ import { BiSearch } from 'react-icons/bi';
 import { FaSort } from 'react-icons/fa';
 
 import { Product } from '../../../../types/product';
+import SearchIcon from '../../shared/icons/SearchIcon';
+import SortIcon from '../../shared/icons/SortIcon';
 
-import FilterSelect from './filterSelect/FilterSelect';
 import { ProductItem } from './product/Product';
 
 type ProductCollectionProps = {
@@ -15,7 +16,6 @@ type ProductCollectionProps = {
 export const ProductCollection = React.memo<ProductCollectionProps>(({ products }) => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
   const [isSortVisible, setIsSortVisible] = useState(false);
-  const [sortedProducts, setSortedProducts] = useState(products);
 
   function handleSearchVisible() {
     setIsSearchVisible((prevState) => !prevState);
@@ -46,7 +46,7 @@ export const ProductCollection = React.memo<ProductCollectionProps>(({ products 
   );
 
   return (
-    <section className="bg-white work-sans py-8">
+    <section className="bg-white worksans py-8">
       <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
         <header className="w-full z-30 top-0 px-6 py-1">
           <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
@@ -54,52 +54,29 @@ export const ProductCollection = React.memo<ProductCollectionProps>(({ products 
               STORE
             </h3>
             <div className="flex items-center">
-              <FilterSelect
-                products={products}
-                setSortedProducts={setSortedProducts}
-                isSortVisible={isSortVisible}
-                styles={sortStylesDesktop}
-              />
               <button
                 onClick={handleSortVisible}
                 className="pl-3 inline-block no-underline text-gray-600 hover:text-black"
               >
-                <svg
-                  className="fill-current hover:text-black"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
-                </svg>
+                <SortIcon />
               </button>
-              <input type="text" className={searchStylesDesktop} placeholder="Search" />
+              <input
+                type="text"
+                className={searchStylesDesktop}
+                placeholder="Search"
+                aria-label="Search"
+              />
               <button
                 onClick={handleSearchVisible}
                 className="pl-3 inline-block no-underline text-gray-600 hover:text-black"
               >
-                <svg
-                  className="fill-current hover:text-black"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
-                </svg>
+                <SearchIcon />
               </button>
             </div>
           </div>
         </header>
-        <FilterSelect
-          products={products}
-          setSortedProducts={setSortedProducts}
-          isSortVisible={isSortVisible}
-          styles={sortStylesMobile}
-        />
         <input type="text" id="searchInput" className={searchStylesMobile} placeholder="Search" />
-        {sortedProducts.map((product) => (
+        {products.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
       </div>
---
+++ apps/www/components/klient/shared/icons/SortIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SortIcon = () => {
+  return (
+    <svg
+      className="fill-current"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
+    </svg>
+  );
+};
+
+export default SortIcon;
