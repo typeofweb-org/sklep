943e2d0f25ddac57b831098513e682a4963c4fc4
Łukasz Dymek <jundymek@gmail.com>
2020-09-08 13:26:38 +0200

    20 (#41)
    * feat: ProductCollection, Product schema
    
    * style: ProductCollection search
    
    * style: ProductCollection search and sort mobile and desktop
    
    * refactor: FilterSelect component
    
    * chore: remove unused code from tsconfig.json
    
    * style: Nordic-Store template
    
    * reafactor: pr 20#41 fixes
    
    * refactor: remove react-placeholder-image, use https://picsum.photos
    
    * reafactor: pr 20#41 fixes 2
    
    * style: add w-full to Product image
    
    * feat: add to cart button next to heart
    
    * style: Product longer name properly displayed
    
    * refactor: remove export default from svg icon components
    
    * feat: add to cart big button
    
    * feat: RenderPrice component
    
    * chore: tailwindcss to dependencies

--- apps/www/components/klient/modules/productCollection/product/Product.tsx
+++ apps/www/components/klient/modules/productCollection/product/Product.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 
 import { Product } from '../../../../../types/product';
+import { HeartIcon } from '../../../shared/icons/HeartIcon';
+import { ShoppingCartIcon } from '../../../shared/icons/ShoppingCartIcon';
+
+import { RenderPrice } from './renderPrice/RenderPrice';
 
 type ProductItemProps = {
   product: Product;
 };
 
 export const ProductItem = React.memo<ProductItemProps>(({ product }) => {
+  const { name, regularPrice, discountPrice } = product;
   return (
-    <div className="">
-      <div key={product.id}>{product.id}</div>
-    </div>
+    <section className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col self-start">
+      <div className="hover:grow hover:shadow-lg">
+        <img src="https://picsum.photos/400" alt="Placeholder" className="w-full" />
+      </div>
+      <button className="bg-gray-900 text-white w-full rounded-sm mt-2 p-2 shadow-sm">
+        <div className="flex justify-center transform transition-transform duration-200 hover:scale-105">
+          <ShoppingCartIcon />
+          <p className="ml-4 self-end">Do koszyka</p>
+        </div>
+      </button>
+      <div className="flex justify-between items-center pt-2">
+        <p className="text-gray-600 pr-2">{name}</p>
+        <div className="flex self-start">
+          <button aria-label="Ulubione" className="pl-3 inline-block" title="Dodaj do ulubionych">
+            <HeartIcon />
+          </button>
+        </div>
+      </div>
+      <RenderPrice regularPrice={regularPrice} discountPrice={discountPrice} />
+    </section>
   );
 });
 ProductItem.displayName = 'ProductItem';
