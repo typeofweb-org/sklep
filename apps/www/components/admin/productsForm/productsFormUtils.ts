63c721226d8ffc2cc6075990c34f81ef71bd3255
WolfOfProgramming <bartekfc17@gmail.com>
2020-09-10 21:59:41 +0200

    Added ResponseError class & Changed Fetcher

--- apps/www/components/admin/productsForm/productsFormUtils.ts
+++ apps/www/components/admin/productsForm/productsFormUtils.ts
@@ -1,6 +1,10 @@
+import { response } from 'msw/lib/types';
+
 import { fetcher } from '../../../utils/fetcher';
 
 import type { ProductType } from './ProductsForm';
 
 export const createProduct = async (payload: ProductType) =>
-  fetcher('http://api.sklep.localhost:3002/products', 'POST', payload);
+  fetcher('http://api.sklep.localhost:3002/products', 'POST', payload).then((response) =>
+    response.json(),
+  );
