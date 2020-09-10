f2bffd9f3bb3b519cfa837a74b61523713ba697e
jundymek <jundymek@gmail.com>
2020-09-10 23:25:24 +0200

    style: Simple LoginForm layout - button on the left side

--- apps/www/components/admin/loginForm/LoginForm.tsx
+++ apps/www/components/admin/loginForm/LoginForm.tsx
@@ -1,50 +1,58 @@
 import { User24 } from '@carbon/icons-react';
-import { Button, Column, FormGroup, Grid, Row, TextInput } from 'carbon-components-react';
+import { Button, Grid, TextInput } from 'carbon-components-react';
 import React from 'react';
+import { Field } from 'react-final-form';
+import * as Yup from 'yup';
 
 import { getErrorProps, ToWForm } from '../../../utils/formUtils';
 
 import styles from './LoginForm.module.scss';
 
-const LoginForm = () => {
-  return (
-    <ToWForm className={styles.form}>
-      <Grid>
-        <h2 className={styles.title}>Log in</h2>
-        <FormGroup>
-          <TextInput
-            id="test2"
-            invalidText="A valid value is required"
-            labelText="Nazwa użytkownika"
-            placeholder="Wpisz nazwę użytkownika"
-          />
-        </FormGroup>
-        <FormGroup>
-          <TextInput.PasswordInput
-            hidePasswordLabel="Hide password"
-            id="test2"
-            invalidText="A valid value is required"
-            labelText="Hasło"
-            placeholder="Podaj hasło użytkownika"
-            showPasswordLabel="Pokaż hasło"
-          />
-        </FormGroup>
-      </Grid>
+const loginSchema = Yup.object({
+  login: Yup.string().required(),
+  password: Yup.string().required(),
+}).required();
+export type LoginType = Yup.InferType<typeof loginSchema>;
 
-      <Grid fullWidth>
-        <Row>
-          <Column>
-            <p>Log in with</p>
-          </Column>
-          <Column>
-            <Button kind="primary" type="submit" renderIcon={User24} className={styles.loginButton}>
-              Zaloguj się
-            </Button>
-          </Column>
-        </Row>
+export const LoginForm = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <ToWForm className={styles.form} onSubmit={handleSubmit} schema={loginSchema}>
+      <Grid>
+        <h2 className={styles.title}>Zaloguj się</h2>
+        <Field name="login">
+          {({ input, meta }) => (
+            <TextInput
+              {...input}
+              {...getErrorProps(meta)}
+              id="login"
+              invalidText="Pole jest wymagane"
+              labelText="Nazwa użytkownika"
+              placeholder="Wpisz nazwę użytkownika"
+            />
+          )}
+        </Field>
+        <Field name="password">
+          {({ input, meta }) => (
+            <TextInput.PasswordInput
+              {...input}
+              {...getErrorProps(meta)}
+              hidePasswordLabel="Hide password"
+              id="password"
+              invalidText="Pole jest wymagane"
+              labelText="Hasło"
+              placeholder="Podaj hasło użytkownika"
+              showPasswordLabel="Pokaż hasło"
+            />
+          )}
+        </Field>
+        <Button kind="primary" type="submit" renderIcon={User24}>
+          Zaloguj się
+        </Button>
       </Grid>
     </ToWForm>
   );
 };
-
-export default LoginForm;
--- apps/www/pages/admin/index.tsx
+++ apps/www/pages/admin/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import React from 'react';
 
 import { Header } from '../../components/admin/Header';
-import LoginForm from '../../components/admin/loginForm/LoginForm';
+import { LoginForm } from '../../components/admin/loginForm/LoginForm';
 import { ProductsForm } from '../../components/admin/productsForm/ProductsForm';
 
 export default function AdminHome() {
