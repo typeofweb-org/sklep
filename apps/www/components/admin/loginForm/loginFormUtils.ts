0c162963bcf7bdd1beabb1253c7ba66d6a6ee85c
jundymek <jundymek@gmail.com>
2020-09-12 17:34:34 +0200

    fix: LoginType import in loginFormUtils

--- apps/www/components/admin/loginForm/loginFormUtils.ts
+++ apps/www/components/admin/loginForm/loginFormUtils.ts
@@ -1,4 +1,6 @@
 import { fetcher } from '../../../utils/fetcher';
 
+import type { LoginType } from './LoginForm';
+
 export const login = async (payload: LoginType) =>
   fetcher('http://api.sklep.localhost:3002/auth/login', 'POST', payload);
