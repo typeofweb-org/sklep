a1f33367761ff5d62729ce68783648845b3f685f
jundymek <jundymek@gmail.com>
2020-09-13 22:04:05 +0200

    Fixes #45

--- apps/www/components/klient/modules/productCollection/product/Product.tsx
+++ apps/www/components/klient/modules/productCollection/product/Product.tsx
@@ -2,8 +2,10 @@ import React, { useCallback } from 'react';
 
 import { Product } from '../../../../../types/product';
 import { HeartIcon } from '../../../shared/icons/HeartIcon';
-import { ShoppingCartIcon } from '../../../shared/icons/ShoppingCartIcon';
 import { Price } from '../../../shared/price/Price';
+
+import { AddToCartButton } from './addToCartButton/AddToCartButton';
+import { ProductImage } from './image/ProductImage';
 
 type ProductItemProps = {
   product: Product;
@@ -16,15 +18,8 @@ export const ProductItem = React.memo<ProductItemProps>(({ product }) => {
 
   return (
     <section className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col self-start">
-      <div className="hover:grow hover:shadow-lg">
-        <img src="https://picsum.photos/400" alt="Placeholder" className="w-full" />
-      </div>
-      <button className="bg-gray-900 hover:bg-gray-800 transform transition-color duration-200 text-white w-full rounded-sm mt-2 shadow-sm">
-        <div className="w-full flex justify-center p-2">
-          <ShoppingCartIcon className={'fill-current'} />
-          <p className="ml-4 self-end">Do koszyka</p>
-        </div>
-      </button>
+      <ProductImage />
+      <AddToCartButton onClick={addToCart} />
       <div className="flex justify-between items-center pt-2">
         <p className="text-gray-600 pr-2">{name}</p>
         <div className="flex self-start">
