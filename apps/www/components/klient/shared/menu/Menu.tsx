7d52a4e1a166f8a2818e5b849bbb6816920f55e1
jundymek <jundymek@gmail.com>
2020-09-14 19:46:36 +0200

    refactor: remove redundant div from Menu

--- apps/www/components/klient/shared/menu/Menu.tsx
+++ apps/www/components/klient/shared/menu/Menu.tsx
@@ -15,20 +15,18 @@ export const Menu = React.memo<MenuProps>(({ isMenuOpen }) => {
   );
 
   return (
-    <div className={menuStyle}>
-      <nav>
-        <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-          <li className="no-underline md:hover:text-black md:hover:underline cursor-pointer">
-            <Link href="/">
-              <a className="flex gap-1 py-2 px-4">
-                <BagIcon className="md:hidden fill-current text-gray-600 hover:text-black cursor-pointer" />
-                <span>Sklep</span>
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={menuStyle}>
+      <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+        <li className="no-underline md:hover:text-black md:hover:underline cursor-pointer">
+          <Link href="/">
+            <a className="flex gap-1 py-2 px-4">
+              <BagIcon className="md:hidden fill-current text-gray-600 hover:text-black cursor-pointer" />
+              <span>Sklep</span>
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 });
 
