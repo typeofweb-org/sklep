f69679c0f8bd9c4d0a974a5593341a87d4c714ed
WolfOfProgramming <47081011+WolfOfProgramming@users.noreply.github.com>
2020-09-14 13:22:22 +0200

    Changed Fetcher & Test file (#64)
    * Changed Fetcher & Test file
    
    * Added Automatic JSON parse in fetcher & Fixed bug with different name for price
    
    * Fixed name in test
    
    * Updated Fetcher
    
    * Updated Fetcher v2
    
    * Make everything typesafe
    
    * Add .env.development
    
    Co-authored-by: Michal Miszczyszyn <michal@mmiszy.pl>

--- apps/www/components/admin/productsForm/ProductsForm.test.tsx
+++ apps/www/components/admin/productsForm/ProductsForm.test.tsx
@@ -8,53 +8,57 @@ import React from 'react';
 import { ProductsForm } from './ProductsForm';
 
 const server = setupServer(
-  rest.post('http://api.sklep.localhost:3002/products', (req, res, ctx) => {
+  rest.post(process.env.NEXT_PUBLIC_API_URL + '/products', (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json({}), ctx.delay(300));
   }),
 );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+describe('form for adding products', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
-test('shows error after confirming without required data', () => {
-  const { getByText } = render(<ProductsForm />);
+  it('shows error after confirming without required data', () => {
+    const { getByText } = render(<ProductsForm />);
 
-  userEvent.click(getByText('Dodaj produkt'));
+    userEvent.click(getByText('Dodaj produkt'));
 
-  expect(getByText('name is a required field')).toBeInTheDocument();
-  expect(getByText('price is a required field')).toBeInTheDocument();
-  expect(getByText('description is a required field')).toBeInTheDocument();
-});
+    expect(getByText('name is a required field')).toBeInTheDocument();
+    expect(getByText('regularPrice is a required field')).toBeInTheDocument();
+    expect(getByText('description is a required field')).toBeInTheDocument();
+  });
 
-test('allows user to add product', async () => {
-  const { getByLabelText, getByText, findByRole } = render(<ProductsForm />);
+  it('allows user to add product', async () => {
+    const { getByLabelText, getByText, findByRole } = render(<ProductsForm />);
 
-  userEvent.type(getByLabelText('Nazwa produktu'), 'Buty XYZ');
-  userEvent.type(getByLabelText('Cena produktu'), '99.9');
-  userEvent.type(getByLabelText('Opis produktu'), 'Dobra rzecz');
+    userEvent.type(getByLabelText('Nazwa produktu'), 'Buty XYZ');
+    userEvent.type(getByLabelText('Cena produktu'), '99.9');
+    userEvent.type(getByLabelText('Opis produktu'), 'Dobra rzecz');
 
-  userEvent.click(getByText('Dodaj produkt'));
+    userEvent.click(getByText('Dodaj produkt'));
 
-  const notification = await findByRole('alert');
-  expect(notification).toHaveTextContent('Dodałeś produkt do bazy danych');
-});
+    const notification = await findByRole('alert');
+    expect(notification).toHaveTextContent('Dodałeś produkt do bazy danych');
+  });
 
-test('shows error notification after bad request server exception', async () => {
-  server.use(
-    rest.post('http://api.sklep.localhost:3002/products', (req, res, ctx) => {
-      return res(ctx.status(403), ctx.delay(300), ctx.json({}));
-    }),
-  );
+  it('shows error notification after bad request server exception', async () => {
+    server.use(
+      rest.post(process.env.NEXT_PUBLIC_API_URL + '/products', (_req, res, ctx) => {
+        return res(ctx.status(400), ctx.delay(300), ctx.json({}));
+      }),
+    );
 
-  const { getByLabelText, getByText, findByRole } = render(<ProductsForm />);
+    const { getByLabelText, getByText, findByRole } = render(<ProductsForm />);
 
-  userEvent.type(getByLabelText('Nazwa produktu'), 'Buty XYZ');
-  userEvent.type(getByLabelText('Cena produktu'), '99.9');
-  userEvent.type(getByLabelText('Opis produktu'), 'Dobra rzecz');
+    userEvent.type(getByLabelText('Nazwa produktu'), 'Buty XYZ');
+    userEvent.type(getByLabelText('Cena produktu'), '99.9');
+    userEvent.type(getByLabelText('Opis produktu'), 'Dobra rzecz');
 
-  userEvent.click(getByText('Dodaj produkt'));
+    userEvent.click(getByText('Dodaj produkt'));
 
-  const notification = await findByRole('alert');
-  expect(notification).toHaveTextContent('Wystąpił błąd podczas dodawania produktu do bazy danych');
+    const notification = await findByRole('alert');
+    expect(notification).toHaveTextContent(
+      'Wystąpił błąd podczas dodawania produktu do bazy danych',
+    );
+  });
 });
