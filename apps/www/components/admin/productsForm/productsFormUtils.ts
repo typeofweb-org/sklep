58d4b68fd50198802f60c2b7128e5a3285e47354
Michal Miszczyszyn <michal@mmiszy.pl>
2020-09-14 20:05:52 +0200

    Typo

--- apps/www/components/admin/productsForm/productsFormUtils.ts
+++ apps/www/components/admin/productsForm/productsFormUtils.ts
@@ -3,8 +3,3 @@ import { fetcher } from '../../../utils/fetcher';
 import type { ProductType } from './ProductsForm';
 
 export const createProduct = async (body: ProductType) => fetcher('/products', 'POST', { body });
-fetcher('/products/{productId}', 'DELETE', {
-  params: {
-    productId: 123,
-  },
-});
