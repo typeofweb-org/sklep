a7bb432d1de37c57c05298bd9fe4eea0f8a80739
jundymek <jundymek@gmail.com>
2020-09-14 10:57:36 +0200

    refactor: move icons from Header to icons/ components

--- apps/www/components/klient/shared/iconLinks/IconLinks.tsx
+++ apps/www/components/klient/shared/iconLinks/IconLinks.tsx
@@ -8,16 +8,12 @@ export const IconLinks = React.memo(() => (
   <div className="order-2 md:order-3 flex items-center">
     <span className="hidden md:contents">
       <Link href="/profil" className="hidden md:inline-block">
-        <a className="inline-block no-underline" aria-label="Koszyk" title="Koszyk">
-          <UserIcon />
-        </a>
+        <UserIcon className="fill-current text-gray-600 hover:text-black cursor-pointer" />
       </Link>
     </span>
 
     <Link href="/koszyk">
-      <a className="pl-3 inline-block no-underline" aria-label="Koszyk" title="Koszyk">
-        <ShoppingCartIcon className={'fill-current text-gray-600 hover:text-black text-6xl'} />
-      </a>
+      <ShoppingCartIcon className="fill-current text-gray-600 hover:text-black text-6xl ml-3 cursor-pointer" />
     </Link>
   </div>
 ));
