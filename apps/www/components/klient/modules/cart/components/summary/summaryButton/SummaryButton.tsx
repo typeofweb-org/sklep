1171827b84160c0f5ddd47273da72a6b8228bad2
jundymek <jundymek@gmail.com>
2020-09-15 23:07:46 +0200

    style: larger text in SummaryButton

--- apps/www/components/klient/modules/cart/components/summary/summaryButton/SummaryButton.tsx
+++ apps/www/components/klient/modules/cart/components/summary/summaryButton/SummaryButton.tsx
@@ -7,7 +7,7 @@ type SummaryButtonProps = {
 export const SummaryButton = React.memo<SummaryButtonProps>(({ onClick }) => {
   return (
     <button
-      className="bg-gray-900 text-white w-full rounded-sm mt-4 p-4 shadow-sm hover:bg-gray-800"
+      className="bg-gray-900 text-white text-lg w-full rounded-sm mt-4 p-4 shadow-sm hover:bg-gray-800"
       onClick={onClick}
       aria-label="Przejdź do płatności"
     >
