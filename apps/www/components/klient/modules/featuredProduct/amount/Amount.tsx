551a7f400e19983fdc174b470f33f95c4577997b
Pawel Chyla <chyla1988@gmail.com>
2020-09-15 22:49:07 +0200

    Polish aria labels

--- apps/www/components/klient/modules/featuredProduct/amount/Amount.tsx
+++ apps/www/components/klient/modules/featuredProduct/amount/Amount.tsx
@@ -8,11 +8,11 @@ type AmountProps = {
 
 export const Amount = React.memo<AmountProps>(({ amount, increaseAmount, decreaseAmount }) => (
   <div className="flex justify-center items-center w-full border-t border-r border-l border-gray-500 py-2 mt-10">
-    <button onClick={decreaseAmount} aria-label="Decrease product amount" className="px-2 mr-2">
+    <button onClick={decreaseAmount} aria-label="Zmniejsz ilość produktu" className="px-2 mr-2">
       &lt;
     </button>
     <p>{amount}</p>
-    <button onClick={increaseAmount} aria-label="Increase product amount" className="px-2 ml-2">
+    <button onClick={increaseAmount} aria-label="Zwiększ ilość produktu" className="px-2 ml-2">
       &gt;
     </button>
   </div>
