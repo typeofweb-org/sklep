232cc82061e3dc3807ebdabe8ecd3926ee6ece1d
jundymek <jundymek@gmail.com>
2020-09-15 22:57:02 +0200

    style: fix smaller screens layout, add px-4 to CartList

--- apps/www/components/klient/modules/cart/components/summary/CartSummary.tsx
+++ apps/www/components/klient/modules/cart/components/summary/CartSummary.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 
+import { SummaryButton } from './summaryButton/SummaryButton';
+
 export const CartSummary = React.memo(() => {
   return (
     <div className="w-full md:w-1/3 mb-4">
-      <h3 className="text-2xl mb-8">Podsumowanie</h3>
+      <h3 className="text-2xl mb-6">Podsumowanie</h3>
       <div className="border border-gray-400 bg-gray-100">
         <div className="flex justify-between p-4 text-2xl border-t border-gray-300">
           <h4>Razem</h4>
           <span>2020 zł</span>
         </div>
       </div>
-      <button
-        className="bg-gray-900 text-white w-full rounded-sm mt-4 p-4 shadow-sm hover:bg-gray-800"
-        title="Przejdź do płatności"
-      >
-        Przejdź do płatności
-      </button>
+      <SummaryButton />
     </div>
   );
 });
