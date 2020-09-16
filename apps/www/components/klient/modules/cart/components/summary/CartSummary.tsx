66e148f9f4d3bd258d4073c6b1cf96c6b8b87f84
jundymek <jundymek@gmail.com>
2020-09-16 11:32:45 +0200

    feat: CheckoutPage

--- apps/www/components/klient/modules/cart/components/summary/CartSummary.tsx
+++ apps/www/components/klient/modules/cart/components/summary/CartSummary.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 export const CartSummary = React.memo(() => {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 mb-4 bg-gray-400">
       <h3 className="text-2xl">Podsumowanie</h3>
+      <Link href="/platnosc">
+        <a>checkout</a>
+      </Link>
     </div>
   );
 });
---
+++ apps/www/pages/platnosc/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Layout } from '../../components/klient/shared/layout/Layout';
+
+function CheckoutPage() {
+  return <Layout title="Płatność i realizacja">To jest to</Layout>;
+}
+
+export default CheckoutPage;
