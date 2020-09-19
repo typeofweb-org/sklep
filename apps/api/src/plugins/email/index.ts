66b9dc561984a1276855403292015cf00c56d05d
wilkadrian <wilkujunior@gmail.com>
2020-09-19 15:25:38 +0200

    Fixed unnecessary async functions declarations

--- apps/api/src/plugins/email/index.ts
+++ apps/api/src/plugins/email/index.ts
@@ -6,7 +6,7 @@ export const AuthPlugin: Hapi.Plugin<EmailPluginOptions> = {
   multiple: false,
   name: 'Sklep Email Plugin',
   version: '1.0.0',
-  async register(server, _options) {
+  register(server, _options) {
     server.events.on('auth:user:registered', (_user) => {
       // @todo send confirmation email
     });
