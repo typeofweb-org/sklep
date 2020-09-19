76c5026f2fbace18951e9bc3d7c7d39fec2b285c
czechue <michal.lester87@gmail.com>
2020-09-19 10:00:29 +0200

    Update apps/www/components/klient/modules/productCollection/components/product/components/description/ProductDescription.tsx
    Co-authored-by: Michał Miszczyszyn <mmiszy@users.noreply.github.com>

--- apps/www/components/klient/modules/productCollection/components/product/components/description/ProductDescription.tsx
+++ apps/www/components/klient/modules/productCollection/components/product/components/description/ProductDescription.tsx
@@ -6,7 +6,7 @@ type ProductDescriptionProps = {
   readonly name: string;
 };
 
-export const ProductDescription = React.memo(({ name }: ProductDescriptionProps) => {
+export const ProductDescription = React.memo<ProductDescriptionProps>(({ name }) => {
   return (
     <div className="flex justify-between items-center pt-2">
       <p className="text-gray-600 pr-2">{name}</p>
