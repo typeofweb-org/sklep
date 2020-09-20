537fb860a1f66c986dfbeefc3eccdcbd5e8b4596
jundymek <jundymek@gmail.com>
2020-09-21 01:33:59 +0200

    style: CheckoutItem price pr-4

--- apps/www/components/klient/modules/checkout/components/item/CheckoutItem.tsx
+++ apps/www/components/klient/modules/checkout/components/item/CheckoutItem.tsx
@@ -28,11 +28,11 @@ export const CheckoutItem = React.memo<CheckoutItemProps>(({ cartItem }) => {
       <td className="w-20 h-24 py-2">
         <CartItemImage />
       </td>
-      <td className="px-2">
+      <td className="px-2 w-2/6 sm:w-1/2">
         <p className="">{product && product.name}</p>
       </td>
       <td className="px-2">x{quantity}</td>
-      <td className="px-2">
+      <td className="pr-4 text-right w-20">
         <p className="">{product && product.regularPrice} zł</p>
       </td>
     </tr>
