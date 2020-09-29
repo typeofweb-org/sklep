b155086b2ddf4b3bf180f8c5c5206681846de6b7
Łukasz Dymek <jundymek@gmail.com>
2020-09-29 19:29:05 +0200

    [Klient] - Testy frontendu - #100 (#116)
    * test: CartStatus.test.tsx
    
    * test:fix: pr116 minor fixes
    
    * test: CartStatus.test.tsx 2 items in cart
    
    * Update AddToCartButton.tsx
    
    * Update CartStatus.test.tsx
    
    * test: HomePage cypress e2e
    
    * test: remove cypress tests
    
    * test: remove cypress tests
    
    Co-authored-by: Michał Miszczyszyn <mmiszy@users.noreply.github.com>

--- apps/www/components/klient/shared/components/cartStatus/CartStatus.tsx
+++ apps/www/components/klient/shared/components/cartStatus/CartStatus.tsx
@@ -13,9 +13,13 @@ export const CartStatus = React.memo(() => {
         <a>
           <ShoppingCartIcon className="fill-current text-gray-600 hover:text-black text-6xl ml-3 cursor-pointer" />
           {itemsInCart > 0 && (
-            <div className="absolute right-0 top-0 -mt-4 -mr-4 rounded-full bg-blue-400 w-5 h-5 text-center text-white text-xs flex items-center justify-center">
+            <span
+              className="absolute right-0 top-0 -mt-4 -mr-4 rounded-full bg-blue-400 w-5 h-5 text-center text-white text-xs flex items-center justify-center"
+              data-testid="cartCounter"
+            >
+              <span className="visually-hidden">Liczba produktów w koszyku: </span>
               {itemsInCart}
-            </div>
+            </span>
           )}
         </a>
       </Link>
