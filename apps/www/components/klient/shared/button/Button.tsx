2e0d7ada27afbe32f0552c6fe4ed2fac70650ee7
Łukasz Dymek <jundymek@gmail.com>
2020-10-12 21:37:19 +0200

    Stripe Elements – na stronie zamówienia 117 (#122)
    * feat: StripePayment.tsx schema
    
    * feat: PaymentMethod show stripe credit card field when check card option
    
    * Add Stripe endpoints #110
    
    * Update plugin
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * feat: checkoutContext
    
    * Fix
    
    * refactor: StripePayment to StripeCard, move Elements to index.ts - zamowienie
    
    * Fix plugin name bug
    
    * feat: initiateStripePayment - StripeCard useEffect
    
    * feat: working Stripe payment - Checkout, StripeCard
    
    * feat: remove unused payment methods - only Card payment
    
    * refactor: remove console logs from Checkout
    
    * refactor: add console.log(values) to Checkout
    
    * refactor: Checkout useMutation
    
    * refactor: Checkout remove context
    
    * style: buttons and CartQuantityInput remove number input spinners
    
    * refactor: StripeCard as final-form Field
    
    * refactor: useStripePayment hook
    
    * refactor: remove initiateStripePayment
    
    * Updates
    
    Co-authored-by: Michal Miszczyszyn <michal@mmiszy.pl>

--- apps/www/components/klient/shared/button/Button.tsx
+++ apps/www/components/klient/shared/button/Button.tsx
@@ -4,7 +4,7 @@ export const Button = React.memo<JSX.IntrinsicElements['button']>(
   ({ className = '', ...props }) => {
     return (
       <button
-        className={`bg-gray-900 text-white text-lg w-full rounded-sm mt-4 p-4 shadow-sm hover:bg-gray-800 ${className}`}
+        className={`bg-gray-900 text-white text-lg w-full rounded-sm mt-4 p-4 h-16 shadow-sm hover:bg-gray-800 ${className}`}
         {...props}
       />
     );
