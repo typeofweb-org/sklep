99586925de7abc42ac1c40939c3b17617ad3c99e
Michał Miszczyszyn <mmiszy@users.noreply.github.com>
2020-10-14 20:11:18 +0200

    Update tests (#118)
    * Update tests
    
    * Fixes to tests
    
    * Fixes to tests
    
    * Removed unnecessary server.use call & resolved conflicts in yarn.lock
    
    * Adding type annotation
    
    Co-authored-by: Bartłomiej <bartekfc17@gmail.com>

--- apps/www/components/admin/loginForm/LoginForm.test.tsx
+++ apps/www/components/admin/loginForm/LoginForm.test.tsx
@@ -2,67 +2,68 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import React from 'react';
 
+import { mswMockServer } from '../../../jest-utils';
 import { ToastsContextProvider } from '../toasts/Toasts';
 
 import { LoginForm } from './LoginForm';
 
-const renderLoginForm = () =>
-  render(
-    <ToastsContextProvider>
-      <LoginForm />
-    </ToastsContextProvider>,
-  );
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+useRouter.mockImplementation(() => ({ replace() {} }));
 
-const server = setupServer(
-  rest.post(process.env.NEXT_PUBLIC_API_URL + '/auth/login', (_req, res, ctx) => {
-    return res(ctx.status(401), ctx.json({}), ctx.delay(300));
-  }),
-);
+describe('login form', () => {
+  const renderLoginForm = () =>
+    render(
+      <ToastsContextProvider>
+        <LoginForm />
+      </ToastsContextProvider>,
+    );
 
-beforeAll(() => {
-  server.listen();
-});
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+  beforeEach(() => {
+    mswMockServer.use(
+      rest.post(process.env.NEXT_PUBLIC_API_URL + '/auth/login', (_req, res, ctx) => {
+        return res(ctx.status(401), ctx.json({}), ctx.delay(300));
+      }),
+    );
+  });
 
-test('shows error after confirming without required data', () => {
-  const { getByText } = renderLoginForm();
+  it('shows error after confirming without required data', () => {
+    const { getByText } = renderLoginForm();
 
-  userEvent.click(getByText('Zaloguj się', { selector: 'button' }));
+    userEvent.click(getByText('Zaloguj się', { selector: 'button' }));
 
-  expect(getByText('Wpisz poprawny adres email')).toBeInTheDocument();
-  expect(getByText('Pole jest wymagane')).toBeInTheDocument();
-});
+    expect(getByText('Wpisz poprawny adres email')).toBeInTheDocument();
+    expect(getByText('Pole jest wymagane')).toBeInTheDocument();
+  });
 
-test('unsuccesfull login', async () => {
-  const { getByLabelText, getByText, findByRole } = renderLoginForm();
+  it('shows error after confirming with improper data', async () => {
+    const { getByLabelText, getByText, findByRole } = renderLoginForm();
 
-  await userEvent.type(getByLabelText('Adres e-mail'), 'testowy@test.pl');
-  await userEvent.type(getByLabelText('Hasło'), 'niepoprawne');
+    await userEvent.type(getByLabelText('Adres e-mail'), 'testowy@test.pl');
+    await userEvent.type(getByLabelText('Hasło'), 'niepoprawne');
 
-  userEvent.click(getByText('Zaloguj się', { selector: 'button' }));
+    userEvent.click(getByText('Zaloguj się', { selector: 'button' }));
 
-  const notification = await findByRole('alert');
-  expect(notification).toHaveTextContent('Wprowadzone dane nie są poprawne');
-});
+    const notification = await findByRole('alert');
+    expect(notification).toHaveTextContent('Wprowadzone dane nie są poprawne');
+  });
 
-test('succesfull login', async () => {
-  server.use(
-    rest.post(process.env.NEXT_PUBLIC_API_URL + '/auth/login', (_req, res, ctx) => {
-      return res(ctx.status(204), ctx.json({}), ctx.delay(300));
-    }),
-  );
+  it('shows notification after successful login', async () => {
+    mswMockServer.use(
+      rest.post(process.env.NEXT_PUBLIC_API_URL + '/auth/login', (_req, res, ctx) => {
+        return res(ctx.status(204), ctx.json({}), ctx.delay(300));
+      }),
+    );
 
-  const { getByLabelText, getByText, findByRole } = renderLoginForm();
+    const { getByLabelText, getByText, findByRole } = renderLoginForm();
 
-  await userEvent.type(getByLabelText('Adres e-mail'), 'test@test1.pl');
-  await userEvent.type(getByLabelText('Hasło'), 'qwertyTESTOWY');
+    await userEvent.type(getByLabelText('Adres e-mail'), 'test@test1.pl');
+    await userEvent.type(getByLabelText('Hasło'), 'qwertyTESTOWY');
 
-  userEvent.click(getByText('Zaloguj się', { selector: 'button' }));
+    userEvent.click(getByText('Zaloguj się', { selector: 'button' }));
 
-  const notification = await findByRole('alert');
-  expect(notification).toHaveTextContent('Logowanie udane');
+    const notification = await findByRole('alert');
+    expect(notification).toHaveTextContent('Logowanie udane');
+  });
 });
