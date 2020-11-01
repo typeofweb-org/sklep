ed7d263cde7d9392c4721cbb887332d3fa867a1e
kajetan <kajetan@na-ma.co.uk>
2020-11-01 16:08:04 +0100

    sprawdzenie statusu zamówienia

---
+++ apps/www/components/admin/adminOrderStatus/adminOrderStatus.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// import { useGetOrderById } from '../../../utils/api/queryHooks';
+
+// type OrderIdProps = {
+//   readonly orderId: string;
+// };
+
+// const ORDER_FINISHED_STATUSES = ['COMPLETED', 'CANCELLED', 'REFUNDED', 'FAILED'];
+
+// export const AdminOrderStatus = React.memo<OrderIdProps>(({ orderId }) => {
+// const [isFinished, setIsFinished] = React.useState(false);
+
+// const getOrderStatus = useGetOrderById(orderId);
+
+//     return (
+//         <div>
+//             <p></p>
+//         </div>
+//     )
+// })
--- apps/www/pages/admin/orders/[orderId].tsx
+++ apps/www/pages/admin/orders/[orderId].tsx
@@ -2,14 +2,17 @@ import Head from 'next/head';
 import React from 'react';
 
 import { AdminLayout } from '../../../components/admin/adminLayout/AdminLayout';
+import { AdminOrderStatus } from '../../../components/admin/adminOrderStatus/adminOrderStatus';
 
-export default function AdminProductsPage() {
+export default function AdminOrderStatusPage() {
   return (
     <>
       <Head>
         <title>Status zamówienia</title>
       </Head>
-      <AdminLayout></AdminLayout>
+      <AdminLayout>
+        <AdminOrderStatus />
+      </AdminLayout>
     </>
   );
 }
