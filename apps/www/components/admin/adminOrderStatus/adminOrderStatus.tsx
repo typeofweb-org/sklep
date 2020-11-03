458e918d273e6b3efb9f728aed0bfdb96e6487f6
kajetan <kajetan@na-ma.co.uk>
2020-11-03 23:05:23 +0100

    dodanie listy ze statusami zamówień

--- apps/www/components/admin/adminOrderStatus/adminOrderStatus.tsx
+++ apps/www/components/admin/adminOrderStatus/adminOrderStatus.tsx
@@ -1,21 +1,35 @@
-// import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
 
-// import { useGetOrderById } from '../../../utils/api/queryHooks';
+import { useGetOrderById } from '../../../utils/api/queryHooks';
 
-// type OrderIdProps = {
-//   readonly orderId: string;
-// };
+export const AdminOrderStatus = React.memo(() => {
+  const router = useRouter();
+  const orderId = String(router.query.orderId);
+  const getOrder = useGetOrderById(orderId);
 
-// const ORDER_FINISHED_STATUSES = ['COMPLETED', 'CANCELLED', 'REFUNDED', 'FAILED'];
+  return (
+    <>
+      <Head>
+        <title>{'Edytuj zamówienie ' + orderId}</title>
+      </Head>
+      <form>
+        <label>
+          Zmień status zamówienie:
+          <select value={getOrder.status}>
+            <option value="PENDING">Pending</option>
+            <option value="PROCESSING">Processing</option>
+            <option value="ON_HOLD">On hold</option>
+            <option value="COMPLETED">Completed</option>
+            <option value="CANCELLED">Cancelled</option>
+            <option value="REFUNDED">Refunded</option>
+            <option value="FAILED">Failed</option>
+          </select>
+        </label>
+      </form>
+    </>
+  );
+});
 
-// export const AdminOrderStatus = React.memo<OrderIdProps>(({ orderId }) => {
-// const [isFinished, setIsFinished] = React.useState(false);
-
-// const getOrderStatus = useGetOrderById(orderId);
-
-//     return (
-//         <div>
-//             <p></p>
-//         </div>
-//     )
-// })
+AdminOrderStatus.displayName = 'AdminOrderStatus';
