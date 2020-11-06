5e4faf4eccf2c8d4e60e8708cae2b78a8f1f0215
Michal Miszczyszyn <michal@mmiszy.pl>
2020-11-06 16:37:08 +0100

    Revert react-final-form

--- apps/api/src/plugins/auth/auth.test.ts
+++ apps/api/src/plugins/auth/auth.test.ts
@@ -59,7 +59,7 @@ describe('/auth', () => {
       expect(injection.result).toHaveProperty('message', 'TOO_EASY');
     });
 
-    it.only(`should return 409 when user with the same email exists`, async () => {
+    it(`should return 409 when user with the same email exists`, async () => {
       const server = await getServerForTest();
 
       const firstName = Faker.name.firstName();
@@ -83,7 +83,6 @@ describe('/auth', () => {
           password: '123!@#dsaASD',
         },
       });
-      console.log(injection.result);
 
       expect(injection.statusCode).toEqual(409);
     });
