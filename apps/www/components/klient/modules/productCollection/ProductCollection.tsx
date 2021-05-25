db5a3ff6b8a15f39f661852c267c2dd4ae7193e2
Michal Miszczyszyn <michal@mmiszy.pl>
2021-05-25 18:55:16 +0200

    Fix

--- apps/www/components/klient/modules/productCollection/ProductCollection.tsx
+++ apps/www/components/klient/modules/productCollection/ProductCollection.tsx
@@ -1,37 +1,33 @@
 import type { SklepTypes } from '@sklep/types';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { ProductItem } from './components/product/Product';
 import { TopBar } from './components/topBar/TopBar';
 
 const filterByLetter = (arr: SklepTypes['getProducts200Response']['data'], value: string) =>
-  arr.filter((item) => item.name.toLowerCase().slice(0, value.length) === value.toLowerCase());
+  value ? arr.filter((item) => item.name.toLowerCase().includes(value.toLowerCase())) : arr;
 
 type ProductCollectionProps = {
   readonly products: SklepTypes['getProducts200Response']['data'];
 };
 
 export const ProductCollection = React.memo<ProductCollectionProps>(({ products }) => {
-  const [items, setItems] = useState(products);
   const [inputValue, setInputValue] = useState('');
 
   const handleSetInputValue = React.useCallback<React.ChangeEventHandler<HTMLInputElement>>((e) => {
     setInputValue(e.target.value);
   }, []);
 
-  useEffect(() => {
-    setItems(filterByLetter(products, inputValue));
-  }, [products, inputValue]);
+  const filteredProducts = useMemo(() => filterByLetter(products, inputValue), [
+    products,
+    inputValue,
+  ]);
 
   return (
     <section className="bg-white worksans p-8 container mx-auto flex items-center flex-wrap">
       <TopBar handleSetInputValue={handleSetInputValue} />
-      {items.length ? (
-        <>
-          {items.map((item) => (
-            <ProductItem key={item.id} product={item} />
-          ))}
-        </>
+      {filteredProducts.length ? (
+        filteredProducts.map((product) => <ProductItem key={product.id} product={product} />)
       ) : (
         <p className="px-6 mt-4">Nie ma takiego produktu...</p>
       )}
